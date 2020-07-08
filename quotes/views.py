7757c8d02a1121427bece6302ed6eb6216e75861
Alex Young <alexyoung612@gmail.com>
2020-07-08 11:12:58 -0700

    added basic detail and index page for quotes

--- quotes/views.py
+++ quotes/views.py
@@ -1,14 +1,15 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
 from .models import Quote
 
 def index(request):
     latest_quote_list = Quote.objects.order_by('-date_created')[:5]
-    output = ', '.join([str(q.pk) for q in latest_quote_list])
-    return HttpResponse(output)
+    context = {'latest_quote_list': latest_quote_list}
+    return render(request, 'quotes/index.html', context)
 
 def detail(request, quote_id):
-    return HttpResponse("You're looking at quote %s." % quote_id)
+    quote = get_object_or_404(Quote, pk=quote_id)
+    return render(request, 'quotes/detail.html', {'quote': quote})
 
 def email_draft(request, quote_id):
     response = "You're looking at the email draft of quote %s."
