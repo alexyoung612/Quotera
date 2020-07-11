5cf5e78fc596627fd7fe558f5251f922627ab3df
Alex Young <alexyoung612@gmail.com>
2020-07-11 13:58:09 -0700

    added main app, base, and navbar

---
+++ main/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def home(response):
+    return render(response, 'main/home.html')
--- quotera/urls.py
+++ quotera/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quotes/', include('quotes.urls'))
+    path('quotes/', include('quotes.urls')),
+    path('', include('main.urls'))
 ]
