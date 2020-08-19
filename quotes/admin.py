68c7631ba695105542a62bc747fe91f5bad42a4d
Alex Young <alexyoung612@gmail.com>
2020-08-19 15:30:44 -0700

    added fixture to seed all initial awning options

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Awning, AwningType, Quote, Customer, QuoteStatus
+from .models import Awning, AwningOperation, AwningType, Quote, Customer, QuoteStatus
 
 admin.site.register(Quote)
 admin.site.register(QuoteStatus)
 admin.site.register(Customer)
 admin.site.register(Awning)
 admin.site.register(AwningType)
+admin.site.register(AwningOperation)
