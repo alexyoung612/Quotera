450a60acfd83d9afd7e13e96106c84f566d3ab20
Alex Young <alexyoung612@gmail.com>
2020-09-10 11:52:20 -0700

    added all awning options to admin panel

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Awning, AwningOperation, AwningType, Quote, Customer, QuoteStatus
+from .models import (
+    Awning, AwningOperation, AwningType, AwningMotorType, AwningCordLength,
+    AwningRemote, AwningCrankSize, AwningHoodCover, AwningValance, AwningMount,
+    AwningBracketType, Quote, Customer, QuoteStatus
+)
 
 admin.site.register(Quote)
 admin.site.register(QuoteStatus)
@@ -8,3 +12,11 @@ admin.site.register(Customer)
 admin.site.register(Awning)
 admin.site.register(AwningType)
 admin.site.register(AwningOperation)
+admin.site.register(AwningMotorType)
+admin.site.register(AwningCordLength)
+admin.site.register(AwningRemote)
+admin.site.register(AwningCrankSize)
+admin.site.register(AwningHoodCover)
+admin.site.register(AwningValance)
+admin.site.register(AwningMount)
+admin.site.register(AwningBracketType)
--- quotes/forms.py
+++ quotes/forms.py
@@ -1,7 +1,9 @@
 import re
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field, Fieldset, Div, HTML, ButtonHolder, Submit, Row
+from crispy_forms.layout import (
+    Layout, Field, Fieldset, Div, HTML, ButtonHolder, Submit, Row
+)
 from crispy_forms.bootstrap import PrependedText
 from django import forms
 from django.forms.models import inlineformset_factory
@@ -100,13 +102,23 @@ class AwningForm(forms.ModelForm):
                     Field('awning_notes', wrapper_class='col-md-10', rows='3'),
                 ),
                 Row(
-                    PrependedText('awning_price_quote', '$', wrapper_class='col-md-3'),
-                    PrependedText('motor_price_quote', '$', wrapper_class='col-md-3'),
-                    PrependedText('bracket_price_quote', '$', wrapper_class='col-md-3'),
-                    PrependedText('additional_costs_quote', '$', wrapper_class='col-md-3'),
+                    PrependedText(
+                        'awning_price_quote', '$', wrapper_class='col-md-3'
+                    ),
+                    PrependedText(
+                        'motor_price_quote', '$', wrapper_class='col-md-3'
+                    ),
+                    PrependedText(
+                        'bracket_price_quote', '$', wrapper_class='col-md-3'
+                    ),
+                    PrependedText(
+                        'additional_costs_quote', '$', wrapper_class='col-md-3'
+                    ),
                 ),
                 Field('DELETE'),
-                css_class='formset_row-{} list-group-item rounded'.format(formtag_prefix)
+                css_class='formset_row-{} list-group-item rounded'.format(
+                    formtag_prefix
+                )
             ),
         )
 
